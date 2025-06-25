195164b846895c7b95ca86c0beccd2cf79292bd2
Skrigan <sk_dm-821@mail.ru>
2025-06-25 21:53:04 +0300

    code review fixes

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
-const datatable_data = [
-  { id: 1, title: "The Shawshank Redemption", year: 1994, votes: 678790, rating: 9.2, rank: 1},
-  { id: 2, title: "The Godfather", year: 1972, votes: 511495, rating: 9.2, rank: 2},
-  { id: 3, title: "The Godfather: Part II", year: 1974, votes: 319352, rating: 9.0, rank: 3},
-  { id: 4, title: "The Good, the Bad and the Ugly", year: 1966, votes: 213030, rating: 8.9, rank: 4},
-  { id: 5, title: "My Fair Lady", year: 1964, votes: 533848, rating: 8.9, rank: 5},
-  { id: 6, title: "12 Angry Men", year: 1957, votes: 164558, rating: 8.9, rank: 6},
-];
-
-const list_data = [
-  { id: 1, value: "Dashboard" },
-  { id: 2, value: "Users" },
-  { id: 3, value: "Products"},
-  { id: 4, value: "Locations"},
-]
+import { datatable_data, list_data } from "./data.js"
 
 const header = {
   view: "toolbar",
@@ -61,10 +47,10 @@ const form = {
     { view: "text", name: "rating", label: "Rating" },
     { view: "text", name: "votes", label: "Votes" },
     {
+      margin: 10,
       cols: [
-        { view: "button", value: "Add new", width: 105, css: "webix_primary" },
-        {},
-        { view: "button", value: "Clear", width: 105 },
+        { view: "button", value: "Add new", css: "webix_primary" },
+        { view: "button", value: "Clear" },
       ],
     },
     {},
